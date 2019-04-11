53fb3ccdd660bf58a8d7833f14422f58acb6acaa
Blake Dietz <blakedietz@gmail.com>
2019-04-11 14:29:10 -0600

    refactor(symbolic-disarray): Make the code a bit cleaner.

---
+++ src/symbolic-disarray/algorithms/index.js
@@ -0,0 +1,20 @@
+import * as d3 from "d3";
+
+export function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+export function createVisData(numberOfColumns, numberOfRows) {
+    const rows = d3.range(1, numberOfRows, 1);
+    const columns = d3.range(1, numberOfColumns, 1);
+    // Create the cartesian product to fill the grid uniformly
+    const points = d3.cross(columns, rows);
+
+    return points.map(([column, row]) => {
+        const φ = row > 2
+            ? getRandomArbitrary(-row, row)
+            : 0;
+
+        return [column, row, φ];
+    });
+}
---
+++ src/symbolic-disarray/symbolic-disarray.store.js
@@ -0,0 +1,46 @@
+import {createVisData} from "./algorithms";
+
+const ACTION_TYPES = {
+    SET_ALGORITHM_PARAMS: "SET_ALGORITHM_PARAMS",
+    SET_UI_STATE: "SET_UI_STATE",
+};
+export const ACTION_CREATORS = {
+    setvisParams: (params) => ({type: ACTION_TYPES.SET_VIS_PARAMS, payload: params}),
+    setUiState: (params) => ({type: ACTION_TYPES.SET_UI_STATE, payload: params})
+};
+
+let initialState = {
+    visParams: {
+        height: 2900,
+        margin: .03,
+        numberOfColumns: 13,
+        numberOfRows: 22,
+        points: [],
+        rotate: 0,
+        sideLength: 100,
+        start: 1,
+        translateX: 0,
+        translateY: 0,
+        width: 2000,
+    },
+    uiState: {
+        drawerOpen: false,
+        debugMode: false,
+    },
+};
+
+initialState.visParams.points = createVisData(initialState.visParams.numberOfColumns, initialState.visParams.numberOfRows);
+
+export {initialState};
+export const reducer = (state, {type, payload}) => {
+    switch (type) {
+        case ACTION_TYPES.SET_VIS_PARAMS:
+            return {...state, visParams: {...state.visParams, ...payload}};
+            break;
+        case ACTION_TYPES.SET_UI_STATE:
+            return {...state, uiState: {...state.uiState, ...payload}};
+            break;
+        default:
+            return {...initialState};
+    }
+};
