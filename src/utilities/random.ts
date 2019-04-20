fab6371d2edc1c79fadb1a41a5c3ec600cecbba4
Blake Dietz <blakedietz@gmail.com>
2019-04-20 17:15:23 -0600

    feat(random.ts): Add array sample method.

--- src/utilities/random.ts
+++ src/utilities/random.ts
@@ -1,3 +1,11 @@
 export function getRandomArbitrary(min: number, max: number): number {
     return Math.random() * (max - min) + min;
 }
+
+/**
+ *
+ * @param givenArray
+ */
+export function sample(givenArray:Array<any>): any {
+    return givenArray[Math.floor(Math.random() * givenArray.length)];
+}
